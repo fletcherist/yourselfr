bf6f55a101e29784ac507f5b50b6a86b43d6ee76
trycatch <romanovexports@gmail.com>
2016-05-13 20:14:40 +0300

    fix error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
   handleChange (locale) {
     cp.setLocale(locale);
     cookie.remove('locale', { path: '/' });
-    cookie.save('alias', locale, { path: '/' });
+    cookie.save('locale', locale, { path: '/' });
   }
 
   render () {
