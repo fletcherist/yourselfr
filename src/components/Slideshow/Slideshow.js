06ead259a0df7db8f80b12373713d6e655f5ad64
phil romanov <romanovexports@gmail.com>
2016-05-29 20:38:50 +0300

    sdfsd

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -5,84 +5,85 @@ import lay2 from './Layout2.jpg';
 import lay3 from './Layout3.jpg';
 import lay4 from './Layout4.jpg';
 import lay5 from './Layout5.jpg';
+import lay6 from './Layout6.jpg';
 
-var backgrounds = [lay1, lay2, lay3, lay4, lay5];
+var backgrounds = [lay1, lay2, lay3, lay4, lay5, lay6];
 
 class Slideshow extends Component {
   constructor () {
     super();
     this.state = {
       style: {
-        background: `url(${backgrounds[0]})`
+        background: `url(${backgrounds[5]})`
       },
       backgroundCount: 1,
       count: 0
     }
   }
-  fadeOut (element) {
-    var opacity = 1;
-
-    var self = this;
-    function decrease () {
-      opacity -= 0.01;
-      if (opacity <= 0) {
-        element.style.opacity = 0;
-
-        var backgroundCount;
-        if (self.state.backgroundCount === backgrounds.length - 1) {
-          backgroundCount = 0;
-        } else {
-          backgroundCount = self.state.backgroundCount + 1;
-        }
-
-        self.setState({
-          style: {
-            background: `url(${backgrounds[self.state.backgroundCount]})`
-          },
-          backgroundCount: backgroundCount,
-          count: self.state.count + 1
-        })
-        self.fadeIn(element);
-        return true;
-      }
-      element.style.opacity = opacity;
-      requestAnimationFrame(decrease);
-    }
-    decrease();
-  }
-
-  fadeIn (element) {
-    var opacity = 0;
-
-    var self = this;
-    function decrease () {
-      opacity += 0.01;
-      if (opacity >= 1) {
-        element.style.opacity = 1;
-
-        setTimeout(() => {
-          self.fadeOut(element);
-        }, 5000);
-        return true;
-      }
-      element.style.opacity = opacity;
-      requestAnimationFrame(decrease);
-    }
-    decrease();
-  }
-  componentDidMount () {
-    console.log(this.state.count);
-    if (this.state.count > 0) {
-      return false;
-    }
-    setTimeout(() => {
-      this.fadeOut(this.background);
-    }, 5000);
-  }
-
-  componentWillUnmount () {
-    return false;
-  }
+  // fadeOut (element) {
+  //   var opacity = 1;
+  //
+  //   var self = this;
+  //   function decrease () {
+  //     opacity -= 0.01;
+  //     if (opacity <= 0) {
+  //       element.style.opacity = 0;
+  //
+  //       var backgroundCount;
+  //       if (self.state.backgroundCount === backgrounds.length - 1) {
+  //         backgroundCount = 0;
+  //       } else {
+  //         backgroundCount = self.state.backgroundCount + 1;
+  //       }
+  //
+  //       self.setState({
+  //         style: {
+  //           background: `url(${backgrounds[self.state.backgroundCount]})`
+  //         },
+  //         backgroundCount: backgroundCount,
+  //         count: self.state.count + 1
+  //       })
+  //       self.fadeIn(element);
+  //       return true;
+  //     }
+  //     element.style.opacity = opacity;
+  //     requestAnimationFrame(decrease);
+  //   }
+  //   decrease();
+  // }
+  //
+  // fadeIn (element) {
+  //   var opacity = 0;
+  //
+  //   var self = this;
+  //   function decrease () {
+  //     opacity += 0.01;
+  //     if (opacity >= 1) {
+  //       element.style.opacity = 1;
+  //
+  //       setTimeout(() => {
+  //         self.fadeOut(element);
+  //       }, 5000);
+  //       return true;
+  //     }
+  //     element.style.opacity = opacity;
+  //     requestAnimationFrame(decrease);
+  //   }
+  //   decrease();
+  // }
+  // componentDidMount () {
+  //   console.log(this.state.count);
+  //   if (this.state.count > 0) {
+  //     return false;
+  //   }
+  //   setTimeout(() => {
+  //     this.fadeOut(this.background);
+  //   }, 5000);
+  // }
+  //
+  // componentWillUnmount () {
+  //   return false;
+  // }
 
   render () {
     return (
@@ -91,7 +92,6 @@ class Slideshow extends Component {
         <div
           className='responsive_crop_fixed'
           style={this.state.style}
-          id='background'
           ref={(c) => this.background = c}>
         </div>
       </div>
