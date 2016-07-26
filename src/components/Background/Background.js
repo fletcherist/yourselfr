503c619dd0d2bb6bcda452cc33edd4f9f61c2223
phil romanov <romanovexports@gmail.com>
2016-07-26 14:58:29 +0300

    fix background

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -6,51 +6,6 @@ class Background extends Component {
   static propTypes = {
     background: PropTypes.string
   }
-  componentWillMount () {
-    this.setState({
-      background: '',
-      filter: ''
-    })
-    this.setBackground()
-  }
-  componentWillReceiveProps () {
-    setTimeout(something => {
-      this.setBackground()
-    }, 300)
-  }
-  setBackground () {
-    console.log(this.props.background)
-    var self = this
-    if (!this.props.background) {
-      self.setState({
-        background: ''
-      })
-      return
-    }
-    var blurred = `${config.http}/upload/background_blur/${this.props.background}`
-    var original = `${config.http}/upload/background/${this.props.background}`
-
-    var imgSmall = new Image()
-    imgSmall.src = blurred
-
-    // 2: load large image
-    var imgLarge = new Image()
-    imgLarge.src = original
-
-    imgSmall.onload = function () {
-      self.setState({
-        background: blurred,
-        filter: 'blur(10px)'
-      })
-
-      imgLarge.onload = function () {
-        self.setState({
-          background: original,
-          filter: 'none'
-        })
-      }
-    }
-  }
   render () {
     if (!this.props.background) {
       return (
@@ -61,9 +16,7 @@ class Background extends Component {
       <div
         className='responsive_crop_fixed'
         style={{
-          background: `url(${this.state.background})`,
-          WebkitFilter: this.state.filter,
-          WebkitTransition: '-webkit-filter .8s'
+          background: `url(${config.http}/upload/background/${this.props.background})`
         }}
         id='background'>
       </div>
